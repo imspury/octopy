c3571b52d2a9a8a5b21d8a88ba560e28278232de
Sam P <s.pur@outlook.com>
2026-01-31 19:55:38 +0000

    Change check_auth to check_conn and check products endpoint

--- src/octopy/client.py
+++ src/octopy/client.py
@@ -8,7 +8,7 @@ class OctoClient:
     """
     A simple client for interacting with the Octopus Energy API.
     """
-    BASE_URL = "https://api.octopus.energy/v1/"
+    BASE_URL = "https://api.octopus.energy/v1"
 
     def __init__(self, api_key: str):
         # Store the API key for authentication
@@ -18,18 +18,13 @@ class OctoClient:
         # API uses basic authentication with the API key as the username
         self.session.auth = (self.api_key, "")
     
-    def check_auth(self, account_number) -> Dict[str, Any]:
+    def check_conn(self) -> Dict[str, Any]:
         """
-        Check if the provided API key is valid by making a request to the
-        account endpoint.
-
-        Args:
-            account_number (str): The account number to check.
+        Check the connection to the Octopus Energy API.
 
         Returns:
-            A dictionary containing account information if the API key is valid.
+            A dictionary containing all currently active products.
         """
-        url = f"{self.BASE_URL}accounts/{account_number}/"
-        response = self.session.get(url)
-        response.raise_for_status()  # Raise an error for bad responses
+        response = self.session.get(f"{self.BASE_URL}/products/")
+        response.raise_for_status()
         return response.json()
