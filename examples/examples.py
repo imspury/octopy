1875848030f862876207cb9527a50991a2204a91
Sam P <s.pur@outlook.com>
2026-02-06 10:54:53 +0000

    Change structure and included logging

--- examples/examples.py
+++ examples/examples.py
@@ -1,62 +1,44 @@
 import os
+import logging
 from dotenv import load_dotenv
 from octopy import OctoClient
 
-# Load configuration from .env file
-load_dotenv()
-API_KEY = os.getenv("OCTOPUS_API_KEY")
-ACCOUNT_NUMBER = os.getenv("OCTOPUS_ACCOUNT_NUMBER")
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 
 def main():
+    # Load configuration from .env file
+    load_dotenv()
+    API_KEY = os.getenv("OCTOPUS_API_KEY")
+    ACCOUNT_NUMBER = os.getenv("OCTOPUS_ACCOUNT_NUMBER")
+
     if not API_KEY or not ACCOUNT_NUMBER:
         print("Missing credentials. Check your .env file.")
         return
 
     # Initialise the client
-    print(f"Starting Octopy...")
     client = OctoClient(api_key=API_KEY)
-    print(f"Octopy initialised!")
+
+    print("Obtainting account information...")
+    account = client.get_account(ACCOUNT_NUMBER)
+    print(f"Success! Found {len(account.properties)} properties")
+
+    for prop in account.properties:
+        print(f"Address: {prop.address_line_1}, {prop.postcode}")
+        print(f"Region: {client.get_region_from_postcode(prop.postcode)}")
     
-    try:
-        # Fetch the account object
-        print(f"Obtainting account information...")
-        account = client.get_account(ACCOUNT_NUMBER)
+        # Electricity Meter Points (works same for gas meter points)
+        for emp in prop.electricity_meter_points:
+            # Find active agreement
+            active_agreement = next((a for a in emp.agreements if a.is_active), None)
 
-        print(f"Success! Found {len(account.properties)} property/properties.")
-
-        for prop in account.properties:
-            print(f"\nAddress: {prop.address_line_1}, {prop.postcode}")
-            print(f"Region: {client.get_region_from_postcode(prop.postcode)}")
-
-            # Electricity Meter Points
-            for emp in prop.electricity_meter_points:
-                # Find active agreement
-                active_agreement = next((a for a in emp.agreements if a.is_active), None)
-
-                print("Electricity:")
-                print(f"- MPAN: {emp.mpan}")
-                if active_agreement:
-                    print(f"- Active Tariff: {active_agreement.tariff_code}")
-                else:
-                    print(f"- No active electricity tariff found.")
-            
-            # Gas Meter Points
-            for gmp in prop.gas_meter_points:
-                # Find active agreement
-                active_agreement = next((a for a in gmp.agreements if a.is_active), None)
-
-                print("Gas:")
-                print(f"- Gas MPRN: {gmp.mprn}")
-                if active_agreement:
-                    print(f"- Active Tariff: {active_agreement.tariff_code}")
-                else:
-                    print(f"- No active gas tariff found.")
-                
-                for meter in gmp.meters:
-                    print(f" - Meter Serial: {meter.serial_number}")
-
-    except Exception as e:
-        print(f"Connection failed: {e}")
+            print(f"Electricity MPAN: {emp.mpan}")
+            if active_agreement:
+                print(f"- Active Tariff: {active_agreement.tariff_code}")
+            else:
+                print(f"- No active electricity tariff found.")
 
 if __name__ == "__main__":
     main()
